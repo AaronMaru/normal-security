a4827bb7a29b6edd993effda3b77c23efb650582
maru <saivichet@skybooking.info>
2019-12-21 16:10:02 +0700

    spring basic security

---
+++ src/main/java/com/example/normalsecurity/config/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.example.normalsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by : maru
+ * Date  : 12/21/2019
+ * Time  : 3:16 PM
+ */
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
---
+++ src/test/java/com/example/normalsecurity/NormalSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.normalsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NormalSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
